e20537069f6a6dd48742b3a441922ee81b6443af
Sanyam Garg <sanyamg2006@gmail.com>
2025-03-06 18:04:55 -0600

    Working and deployed project

--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Hyperparameters
 batch_size = 64
 learning_rate = 0.001
-num_epochs = 20
+num_epochs = 100
 
 # MNIST dataset and data loaders
 transform = transforms.Compose([
